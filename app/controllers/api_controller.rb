5c25d7fc24328e17b32835c1175b7fc042009734
Gabriele Rulli <rulligabri.95@gmail.com>
2019-05-05 20:10:21 +0200

    Add token authentication middleware to api controllers

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,3 +1,20 @@
 class ApiController < ApplicationController
-  before_action :require_login
+  before_action :authenticate_via_token
+  
+  def authenticate_via_token
+    unless api_token
+      render json: { errors: 'Authentication failed, try call "/login" endpoint' }, status: 401
+    end
+    user = User.find_by_remember_token(api_token)
+    sign_in user if user
+    cookies.delete(:remember_token) # so non-browser clients don't act like browsers and persist sessions in cookies
+  end
+
+  private
+
+  def api_token
+    pattern = /^Bearer /
+    header  = request.env["HTTP_AUTHORIZATION"]
+    header.gsub(pattern, '') if header && header.match(pattern)
+  end
 end
